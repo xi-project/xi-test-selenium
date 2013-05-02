1661b74158ceb5c9534155bc4c77e8f4b63377ef
Martin Pärtel <martin.partel@gmail.com>
2013-05-02 17:13:39 +0300

    Fix race condition in tests/run-tests.sh.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -19,9 +19,11 @@ trap cleanup $SIGNALS
 if [ "$1" = '-n' ]; then
     shift
 else
+    rm -f selenium.log  # if we didn't, we could race to the grep before the old file is replaced
     ./selenium-server.sh > selenium.log 2>&1 &
     SELENIUM_PID=$!
     echo "Started Selenium server, pid = $SELENIUM_PID"
+
     echo -n "Waiting for Selenium server to get up"
     until `grep -q "Started org.openqa.jetty.jetty.Server" selenium.log`; do
         sleep 0.5
